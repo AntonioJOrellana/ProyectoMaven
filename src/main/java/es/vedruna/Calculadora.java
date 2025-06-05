a5ac0522a2233135fde3a377c05d3698e552c51d
AntonioJesusOrellana <antoniojesus.orellana@a.vedrunasevillasj.es>
2025-06-05 20:30:57 +0200

    Aplicacion terminada.

--- src/main/java/es/vedruna/Calculadora.java
+++ src/main/java/es/vedruna/Calculadora.java
@@ -2,15 +2,19 @@ package es.vedruna;
 
 public class Calculadora {
 
+    //Este metodo hace una suma de dos double
     public double sumar(double number1, double number2){
         return number1 + number2;
     }
+    //Este metodo hace una resta de dos double
     public double restar(double number1, double number2){
         return number1 - number2;
     }
+    //Este metodo hace una multiplicacion de dos double
     public double multiplicar(double number1, double number2){
         return number1 * number2;
     }
+    //Este metodo hace una division de dos double
     public double dividir(double number1, double number2){
         return number1 / number2;
     }
